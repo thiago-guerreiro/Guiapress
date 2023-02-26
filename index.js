c5d4cdc5f668bdc6aef663e4e1ea4da3de75ff8b
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-02-26 11:37:55 -0300

    CategoriesController e ArticlesController

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 // view engine
 app.set('view engine', 'ejs');
 
@@ -22,6 +25,10 @@ connection
     .catch((error) => {
         console.log(error);
     })
+
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 
 app.get("/", (req, res) => {
