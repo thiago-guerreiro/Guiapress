0e85187075ec0e3ca17bb41c91ee2696642b1116
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-02-28 20:22:31 -0300

    Listagem de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -26,4 +26,10 @@ router.post("/categories/save", (req, res) => {
     }
 });
 
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories});
+    });
+});
+
 module.exports = router;
